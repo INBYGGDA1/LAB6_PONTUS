7f0010efd6e43e4958b971d431d06c23598abf46
pontus <github@doughnut.mailer.me>
2023-10-07 00:06:00 +0200

    Working like a charm

--- Assignment_4.3/src/main.c
+++ Assignment_4.3/src/main.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
 
 /*================================================================*/
 #include "driverlib/gpio.h"
@@ -10,8 +11,10 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/adc.h"
 #include "driverlib/uart.h"
+#include "driverlib/interrupt.h"
 #include "drivers/buttons.h"
 #include "drivers/pinout.h"
+#include "inc/hw_ints.h"
 #include "utils/uartstdio.h"
 
 /*================================================================*/
@@ -28,7 +31,11 @@
 #include "queue.h"
 #include "portable.h"
 
-#define WAIT_TIME 20
+#define WAIT_TIME 5
+#define MIC_SAMPLES 8
+#define JOY_SAMPLES 4
+#define ACC_SAMPLES 2
+
 // If below 50 then joystick-x and microphone becomes correlated
 #define BLOCK_TIME 50
 
@@ -37,16 +44,16 @@ QueueHandle_t mic_queue, joy_queue, acc_queue;
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 struct microphone_values {
-  uint32_t mic_val[8];
+  uint32_t mic_val[MIC_SAMPLES];
 };
 struct joystick_values {
-  uint32_t joy_x[4];
-  uint32_t joy_y[4];
+  uint32_t joy_x[JOY_SAMPLES];
+  uint32_t joy_y[JOY_SAMPLES];
 };
 struct accelerometer_values {
-  uint32_t acc_x[2];
-  uint32_t acc_y[2];
-  uint32_t acc_z[2];
+  uint32_t acc_x[ACC_SAMPLES];
+  uint32_t acc_y[ACC_SAMPLES];
+  uint32_t acc_z[ACC_SAMPLES];
 };
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -72,24 +79,45 @@ void ConfigureUART(void) {
 }
 //=============================================================================
 // Initialize ADC0 for accelerometer (gyroscope)
+void ADC_newSequence(uint32_t ui32base, uint32_t ui32SequenceNum,
+                     uint32_t ui32ADC_Channel, uint32_t ui32Samples) {
+
+  int i;
+  ADCIntEnable(ui32base, ui32SequenceNum);
+  ADCIntClear(ui32base, ui32SequenceNum);
+  ADCSequenceDisable(ui32base, ui32SequenceNum);
+  ADCSequenceConfigure(ui32base, ui32SequenceNum, ADC_TRIGGER_PROCESSOR, 0);
+  for (i = 0; i < ui32Samples; i++) {
+
+    ADCSequenceStepConfigure(ui32base, ui32SequenceNum, i, ui32ADC_Channel);
+  }
+  ADCSequenceStepConfigure(ui32base, ui32SequenceNum, ui32Samples - 1,
+                           ADC_CTL_END | ADC_CTL_IE | ui32ADC_Channel);
+  ADCSequenceEnable(ui32base, ui32SequenceNum);
+}
 void initialize_adc0(void) {
+  int i;
   // Enable the ADC0 module.
   SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+  SysCtlDelay(10);
   while (!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)) {
   }
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);
-  while (!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC1)) {
-  }
+
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+  SysCtlDelay(10);
   while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)) {
   }
+
+  GPIOPinTypeADC(
+      GPIO_PORTE_BASE,
+      GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 |
+          GPIO_PIN_5); // Enable all pins that should be set as ADC pins
 }
 //=============================================================================
 
 void task_mic(void *pvParameters) {
 
-  int T = 1;
-  int i = 0;
+  const int T = 1;
   int samplesReturned = 0;
 
   const TickType_t xDelay = pdMS_TO_TICKS(WAIT_TIME * T); // 10s delay
@@ -99,192 +127,127 @@ void task_mic(void *pvParameters) {
 
   for (;;) {
     vTaskDelayUntil(&xLasWakeTime, xDelay); // Periodicity with 1
+    ADC_newSequence(ADC0_BASE, 0, ADC_CTL_CH8, MIC_SAMPLES);
 
-    int toSend = 0;
-    for (i = 0; i < 8; i++) {
-      mic_val->mic_val[i] = 0;
-    }
-    // Sample some data
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
-    ADCSequenceConfigure(ADC1_BASE, 0, ADC_TRIGGER_PROCESSOR,
-                         3); // A sequence of 0 collects 8 samples
-    ADCSequenceStepConfigure(ADC1_BASE, 0, 7,
-                             ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH9);
-    ADCSequenceEnable(ADC1_BASE, 0); // Enable the ADC sequence
-    // ADCIntEnable(ADC1_BASE, 0);
     ADCProcessorTrigger(
-        ADC1_BASE,
+        ADC0_BASE,
         0); // // Wait until the sample sequence has completed. //
-    while (!ADCIntStatus(ADC1_BASE, 0, false)) {
-    } // // Read the
-      // value from the ADC.
-      // for (i = 0; i < 8; i++) {
-
+    while (!ADCIntStatus(ADC0_BASE, 0, false)) {
+    }
     samplesReturned = ADCSequenceDataGet(
-        ADC1_BASE, 0,
+        ADC0_BASE, 0,
         mic_val->mic_val); // Providing the full array should place all the
                            // value in FIFO in the array (Up to 8 values)
-    // }
-    for (i = 0; i < 8; i++) {
-      if (mic_val->mic_val[i] != 0) {
-        toSend++;
+    ADCIntClear(ADC0_BASE, 0);
+
+    if (samplesReturned == MIC_SAMPLES) {
+      while (xQueueSend(mic_queue, mic_val, portMAX_DELAY) != pdTRUE) {
       }
-    }
-    xQueueSend(mic_queue, &mic_val, 50);
-    if (toSend == 8) {
     }
   }
 }
 void task_joy(void *pvParameters) {
-  int T = 2;
-  int i;
+  const int T = 2;
+  int i = 0;
   int samplesReturned = 0;
-  const TickType_t xDelay =
-      pdMS_TO_TICKS(WAIT_TIME * T); // 10s delay
+  uint32_t joyValuesRead[2 * JOY_SAMPLES];
+  const TickType_t xDelay = pdMS_TO_TICKS(WAIT_TIME * T);
   TickType_t xLasWakeTime = xTaskGetTickCount();
   struct joystick_values *joy = pvPortMalloc(sizeof(struct joystick_values));
+
   for (;;) {
-    vTaskDelayUntil(&xLasWakeTime, xDelay); // Periodicity with 1
-    int toSend = 0;
-    for (i = 0; i < 4; i++) {
-      joy->joy_x[i] = 5000;
-      joy->joy_y[i] = 5000;
-    }
+    vTaskDelayUntil(&xLasWakeTime, xDelay); // Periodicity with 2
+    samplesReturned = 0;
 
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_4);
-    // Sample some data
-    ADCSequenceConfigure(ADC1_BASE, 0, ADC_TRIGGER_PROCESSOR,
-                         2); // A sequence of 2, captures up to 4 samples
-    ADCSequenceStepConfigure(ADC1_BASE, 0, 7,
-                             ADC_CTL_IE | ADC_CTL_END |
-                                 ADC_CTL_CH0); // Since the buffer is of size 0,
-                                               // 0 samples will get discarded
-    ADCSequenceEnable(ADC1_BASE, 0);
-    // ADCIntEnable(ADC1_BASE, 0);
-    ADCProcessorTrigger(ADC1_BASE, 0);
-    while (!ADCIntStatus(ADC1_BASE, 0, false)) {
+    ADC_newSequence(ADC0_BASE, 1, ADC_CTL_CH9, JOY_SAMPLES); // X-value
+    ADCProcessorTrigger(ADC0_BASE, 1);
+    while (!ADCIntStatus(ADC0_BASE, 1, false)) {
     }
-    samplesReturned = ADCSequenceDataGet(ADC1_BASE, 0, joy->joy_x);
+    samplesReturned = ADCSequenceDataGet(ADC0_BASE, 1, joy->joy_x);
+    ADCIntClear(ADC0_BASE, 1);
 
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_5);
-    // ADCSequenceConfigure(ADC1_BASE, 0, ADC_TRIGGER_PROCESSOR,
-    //                      0); // A sequence of 0, captures up to 4 samples
-    ADCSequenceStepConfigure(ADC1_BASE, 0, 7,
-                             ADC_CTL_IE | ADC_CTL_END |
-                                 ADC_CTL_CH8); // Channel 8
-    ADCSequenceEnable(ADC1_BASE,
-                      0); // Enable the sequence for channel 8
-    // ADCIntEnable(ADC1_BASE, 0);
-    ADCProcessorTrigger(ADC1_BASE, 0); // Trigger the sampling
-    while (!ADCIntStatus(ADC1_BASE, 0, false)) {
+    ADC_newSequence(ADC0_BASE, 1, ADC_CTL_CH0, JOY_SAMPLES); // Y-value
+    ADCProcessorTrigger(ADC0_BASE, 1);
+    while (!ADCIntStatus(ADC0_BASE, 1, false)) {
     }
-    ADCSequenceDataGet(ADC1_BASE, 0,
-                       joy->joy_y); // Place the data in the y buffer
-    for (i = 0; i < 4; i++) {       // Check so that all arrays have a value
-      if (joy->joy_x[i] != 5000 && joy->joy_y[i] != 5000) {
-        toSend++;
+    samplesReturned += ADCSequenceDataGet(ADC0_BASE, 1, joy->joy_y);
+    ADCIntClear(ADC0_BASE, 1);
+
+    if (samplesReturned == 8) {
+      while (xQueueSend(joy_queue, joy, portMAX_DELAY) != pdTRUE) {
       }
-    }
-    xQueueSend(joy_queue, &joy, 50);
-    if (toSend == 4) {
-      // Dont send
     }
   }
   vPortFree(joy);
 }
 
 void task_acc(void *pvParameters) {
-  int T = 4;
-  int i;
+  const int T = 4;
+  int i = 0;
+  int samplesReturned = 0;
+  uint32_t accValuesRead[3 * ACC_SAMPLES];
   const TickType_t xDelay = pdMS_TO_TICKS(WAIT_TIME * T); // 10s delay
   TickType_t xLasWakeTime = xTaskGetTickCount();
   struct accelerometer_values *acc =
       pvPortMalloc(sizeof(struct accelerometer_values));
   for (;;) {
     vTaskDelayUntil(&xLasWakeTime, xDelay); // Periodicity with 1
-    int toSend = 0;
-    for (i = 0; i < 2; i++) {
-      acc->acc_x[i] = 0;
-      acc->acc_y[i] = 0;
-      acc->acc_z[i] = 0;
+    samplesReturned = 0;
+    ADC_newSequence(ADC0_BASE, 1, ADC_CTL_CH3, ACC_SAMPLES); // X-value
+    ADCProcessorTrigger(ADC0_BASE, 1);
+    while (!ADCIntStatus(ADC0_BASE, 1, false)) {
     }
+    samplesReturned += ADCSequenceDataGet(ADC0_BASE, 1, acc->acc_x);
+    ADCIntClear(ADC0_BASE, 1);
 
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_1);
-    ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR,
-                         0); // A sequence of 2, captures up to 4 samples
-    ADCSequenceStepConfigure(ADC0_BASE, 2, 1,
-                             ADC_CTL_IE | ADC_CTL_END |
-                                 ADC_CTL_CH2); // Channel 8
-    ADCSequenceEnable(ADC0_BASE,
-                      2); // Enable the sequence for channel 8
-    // ADCIntEnable(ADC1_BASE, 2);
-    ADCProcessorTrigger(ADC0_BASE, 2); // Trigger the sampling
-    while (!ADCIntStatus(ADC0_BASE, 2, false)) {
+    ADC_newSequence(ADC0_BASE, 1, ADC_CTL_CH2, ACC_SAMPLES); // Y-value
+    ADCProcessorTrigger(ADC0_BASE, 1);
+    while (!ADCIntStatus(ADC0_BASE, 1, false)) {
     }
-    ADCSequenceDataGet(ADC0_BASE, 2,
-                       acc->acc_x); // Place the data in the x buffer
+    samplesReturned += ADCSequenceDataGet(ADC0_BASE, 1, acc->acc_y);
+    ADCIntClear(ADC0_BASE, 1);
 
-    // Sample some data
-
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
-    // ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR,
-    //                      0); // A sequence of 2, captures up to 4 samples
-    ADCSequenceStepConfigure(ADC0_BASE, 2, 1,
-                             ADC_CTL_IE | ADC_CTL_END |
-                                 ADC_CTL_CH1); // Channel 8
-    ADCSequenceEnable(ADC0_BASE,
-                      2); // Enable the sequence for channel 8
-    // ADCIntEnable(ADC1_BASE, 2);
-    ADCProcessorTrigger(ADC0_BASE, 2); // Trigger the sampling
-    while (!ADCIntStatus(ADC0_BASE, 2, false)) {
+    ADC_newSequence(ADC0_BASE, 1, ADC_CTL_CH1, ACC_SAMPLES); // Z-value
+    ADCProcessorTrigger(ADC0_BASE, 1);
+    while (!ADCIntStatus(ADC0_BASE, 1, false)) {
     }
-    ADCSequenceDataGet(ADC0_BASE, 2,
-                       acc->acc_y); // Place the data in the x buffer
+    samplesReturned += ADCSequenceDataGet(ADC0_BASE, 1, acc->acc_z);
+    ADCIntClear(ADC0_BASE, 1);
 
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_0);
-    // ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR,
-    //                      0); // A sequence of 2, captures up to 4 samples
-    ADCSequenceStepConfigure(ADC0_BASE, 2, 1,
-                             ADC_CTL_IE | ADC_CTL_END |
-                                 ADC_CTL_CH1); // Channel 8
-    ADCSequenceEnable(ADC0_BASE,
-                      2); // Enable the sequence for channel 8
-    // ADCIntEnable(ADC1_BASE, 2);
-    ADCProcessorTrigger(ADC0_BASE, 2); // Trigger the sampling
-    while (!ADCIntStatus(ADC0_BASE, 2, false)) {
-    }
-    ADCSequenceDataGet(ADC0_BASE, 2,
-                       acc->acc_z); // Place the data in the x buffer
-
-    for (i = 0; i < 2; i++) {
-      if (acc->acc_x[i] != 0 && acc->acc_y[i] != 0 && acc->acc_z[i] != 0) {
-        toSend++;
+    if (samplesReturned == ACC_SAMPLES * 3) {
+      while (xQueueSend(acc_queue, acc, portMAX_DELAY) != pdTRUE) {
       }
-    }
-    xQueueSend(acc_queue, &acc, 50);
-    if (toSend == 2) {
-      // Dont send
-      // Unable to send
     }
   }
   vPortFree(acc);
 }
 void xGatekeeper(void *pvParameters) {
-  int T = 8;
-  int i;
+  const int T = 8;
+  int i = 0;
   int micAverageTemp = 0;
   int micAverage = 0;
   int micAverageTodB = 0;
-  int joyAverageXtemp = 0, joyAverageYtemp = 0;
-  int joyAverageX = 0, joyAverageY = 0;
-  int accAverageXtemp = 0, accAverageYtemp = 0, accAverageZtemp = 0;
-  int accAverageX = 0, accAverageY = 0, accAverageZ = 0;
-  BaseType_t micQueueReturned, joyQueueReturned, accQueueReturned;
+  int joyAverageXtemp = 0;
+  int joyAverageYtemp = 0;
+  int joyAverageX = 0;
+  int joyAverageY = 0;
+  int accAverageXtemp = 0;
+  int accAverageYtemp = 0;
+  int accAverageZtemp = 0;
+  int accAverageX = 0;
+  int accAverageY = 0;
+  int accAverageZ = 0;
+  int toPrintOrNotToPrint = 0;
+  BaseType_t micQueueReturned;
+  BaseType_t joyQueueReturned;
+  BaseType_t accQueueReturned;
+
   struct joystick_values *joy = pvPortMalloc(sizeof(struct joystick_values));
   struct accelerometer_values *acc =
       pvPortMalloc(sizeof(struct accelerometer_values));
   struct microphone_values *mic_val =
       pvPortMalloc(sizeof(struct microphone_values));
+
   const TickType_t xDelay = pdMS_TO_TICKS(5 * T); // 10s delay
   TickType_t xLasWakeTime = xTaskGetTickCount();
 
@@ -292,6 +255,7 @@ void xGatekeeper(void *pvParameters) {
 
     vTaskDelayUntil(&xLasWakeTime, xDelay);
 
+    toPrintOrNotToPrint = 0;
     micAverageTemp = 0;
     joyAverageXtemp = 0;
     joyAverageYtemp = 0;
@@ -299,68 +263,48 @@ void xGatekeeper(void *pvParameters) {
     accAverageYtemp = 0;
     accAverageZtemp = 0;
 
-    int toPrintOrNotToPrint = 0;
-    micQueueReturned = xQueueReceive(mic_queue, &mic_val, portMAX_DELAY);
+    micQueueReturned = xQueueReceive(mic_queue, mic_val, 0);
     if (micQueueReturned == pdPASS) {
-      // UARTprintf("MIC: ");
-      // for (i = 0; i < 8; i++) {
-      //
-      //   UARTprintf("%d ", mic_val->mic_val[i]);
-      // }
-      // UARTprintf("\n");
-      for (i = 0; i < 8; i++) {
+      for (i = 0; i < MIC_SAMPLES; i++) {
         micAverageTemp += mic_val->mic_val[i];
       }
-      micAverage = micAverageTemp / 8;
+      micAverage = micAverageTemp / MIC_SAMPLES;
       micAverageTodB = round(20.0 * log10(micAverage));
       toPrintOrNotToPrint = 1;
     }
-    joyQueueReturned = xQueueReceive(joy_queue, &joy, portMAX_DELAY);
+    joyQueueReturned = xQueueReceive(joy_queue, joy, 0);
     if (joyQueueReturned == pdPASS) {
-      // UARTprintf("JOY-X: ");
-      // for (i = 0; i < 4; i++) {
-      //
-      //   UARTprintf("%d ", joy->joy_x[i]);
-      // }
-      //
-      // UARTprintf("\n");
-      // UARTprintf("JOY-Y: ");
-      // for (i = 0; i < 4; i++) {
-      //
-      //   UARTprintf("%d ", joy->joy_y[i]);
-      // }
-      // UARTprintf("\n");
-      for (i = 0; i < 4; i++) {
+      for (i = 0; i < JOY_SAMPLES; i++) {
         joyAverageXtemp += joy->joy_x[i];
         joyAverageYtemp += joy->joy_y[i];
       }
-      joyAverageX = joyAverageXtemp / 4;
-      joyAverageY = joyAverageYtemp / 4;
+      joyAverageX = joyAverageXtemp / JOY_SAMPLES;
+      joyAverageY = joyAverageYtemp / JOY_SAMPLES;
       toPrintOrNotToPrint = 1;
     }
-    accQueueReturned = xQueueReceive(acc_queue, &acc, portMAX_DELAY);
+    accQueueReturned = xQueueReceive(acc_queue, acc, 0);
     if (accQueueReturned == pdPASS) {
-      for (i = 0; i < 2; i++) {
+      for (i = 0; i < ACC_SAMPLES; i++) {
         accAverageXtemp += acc->acc_x[i];
         accAverageYtemp += acc->acc_y[i];
         accAverageZtemp += acc->acc_z[i];
       }
-      accAverageX = accAverageXtemp / 2;
-      accAverageY = accAverageYtemp / 2;
-      accAverageZ = accAverageZtemp / 2;
+      accAverageX = accAverageXtemp / ACC_SAMPLES;
+      accAverageY = accAverageYtemp / ACC_SAMPLES;
+      accAverageZ = accAverageZtemp / ACC_SAMPLES;
       toPrintOrNotToPrint = 1;
     }
 
     if (toPrintOrNotToPrint) {
       UARTprintf("\033[2J");
-      // UARTprintf("MIC: %d, dB\n", micAverageTodB);
-      // UARTprintf("ACC: %d x, %d y, %d z\n", accAverageX, accAverageY,
-      //            accAverageZ);
-      // UARTprintf("JOY: %d x, %d y\n", joyAverageX, joyAverageY);
-      UARTprintf("MIC: %d, dB\n", mic_val->mic_val[0]);
-      UARTprintf("ACC: %d x, %d y, %d z\n", acc->acc_x[0], acc->acc_y[0],
-                 acc->acc_z[0]);
-      UARTprintf("JOY: %d x, %d y\n", joy->joy_x[0], joy->joy_y[0]);
+      UARTprintf("MIC: %d, dB\n", micAverageTodB);
+      UARTprintf("ACC: %d x, %d y, %d z\n", accAverageX, accAverageY,
+                 accAverageZ);
+      UARTprintf("JOY: %d x, %d y\n", joyAverageX, joyAverageY);
+      // UARTprintf("MIC: %d, dB\n", mic_val->mic_val[0]);
+      // UARTprintf("ACC: %d x, %d y, %d z\n", acc->acc_x[0], acc->acc_y[0],
+      //            acc->acc_z[0]);
+      // UARTprintf("JOY: %d x, %d y\n", joy->joy_x[0], joy->joy_y[0]);
     }
   }
   vPortFree(mic_val);
@@ -368,6 +312,11 @@ void xGatekeeper(void *pvParameters) {
   vPortFree(joy);
 }
 int main(void) {
+  uint32_t systemClock;
+  systemClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_INT |
+                                    SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
+                                   120000000);
+
   int queueSize = 3;
   TaskHandle_t xTaskMicHandle, xTaskAccHandle, xTaskJoyHandle,
       xGatekeerperHandle;
@@ -380,9 +329,9 @@ int main(void) {
   UARTprintf("\033[2J");
   initialize_adc0();
 
-  mic_queue = xQueueCreate(queueSize, sizeof(&mic));
-  joy_queue = xQueueCreate(queueSize, sizeof(&joy));
-  acc_queue = xQueueCreate(queueSize, sizeof(&acc));
+  mic_queue = xQueueCreate(queueSize, sizeof(mic));
+  joy_queue = xQueueCreate(queueSize, sizeof(joy));
+  acc_queue = xQueueCreate(queueSize, sizeof(acc));
 
   if (mic_queue != NULL && joy_queue != NULL && acc_queue != NULL) {
     UARTprintf("Queues succesfully created\n");
